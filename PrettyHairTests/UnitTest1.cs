b277eb7318ba85e09a4097d827f2e236b0d93f48
sist <sist@EAL-01055>
2016-12-02 13:53:45 +0100

    Add project files.

---
+++ PrettyHairTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using PrettyHairBL;
+
+namespace PrettyHairTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Classa c = new Classa();
+            
+        }
+    }
+}
---
+++ PrettyHairTests2/PrettyHairTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrettyHairTests2
+{
+    [TestClass]
+    public class PrettyHairTests
+    {
+       
+        [TestMethod]
+        public void CheckStock_GetProducts_ReturnListOfProducts()
+        {
+
+        }
+        [TestMethod]
+        public void CheckStock_CheckStockForProductsMarkedUnsellable_ReturnListOfProducts()
+        {
+
+        }
+        [TestMethod]
+        public void CheckStock_DeleteProducts_ProductListIsOneShorterThanBefore()
+        {
+
+        }
+        [TestMethod]
+        public void CheckStock_AdjustPriceOnProduct_PriceHasChanged()
+        {
+
+        }
+        [TestMethod]
+        public void CheckStock_AdjustDescriptionOnProduct_DescriptionHasChanged()
+        {
+
+        }
+    }
+}
